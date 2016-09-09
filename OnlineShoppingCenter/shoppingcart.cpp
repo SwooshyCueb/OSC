4c23acb9c9ec5e5116c88e24a273ba5430197d15
justin <justin@localhost.localdomain>
2016-09-09 11:07:45 -0500

    shopping classes

--- OnlineShoppingCenter/shoppingcart.cpp
+++ OnlineShoppingCenter/shoppingcart.cpp
@@ -1,6 +1,79 @@
 #include "shoppingcart.h"
+#include "product.h"
+#include <iostream>
+#include <algorithm>
 
 ShoppingCart::ShoppingCart()
 {
 
+}
+ShoppingCart::ShoppingCart(Product p){
+    cart.push_back(p);
+    product_count[p.getUPC()] = 1;
+
+}
+int ShoppingCart::addProduct(Product p) {
+
+
+    if (ShoppingCart::checkCart(p) <  1) {
+        cart.push_back(p);
+        product_count[p.getUPC()] = 1;
+    }
+    else {
+        product_count[p.getUPC()] = product_count[p.getUPC()] + 1;
+    }
+
+    return 1;
+}
+
+int ShoppingCart::getCount(Product p) {
+    return product_count.count(p.getUPC());
+}
+
+int ShoppingCart::checkCart(Product p) {
+    if (product_count.count(p.getUPC()) > 0) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+int ShoppingCart::emptyCart() {
+    cart.clear();
+    product_count.clear();
+    std::cout << "You have emptied your shopping cart." << std::endl;
+    return 1;
+}
+
+int ShoppingCart::deleteProduct(Product p) {
+    if (product_count.count(p.getUPC()) < 1) {
+        return -1;
+    }
+    else if (product_count.count(p.getUPC()) >= 1) {
+        product_count.erase(p.getUPC());
+
+
+        for (int i = 0; i < cart.size(); i++) {
+            if (p.getUPC() == cart[i].getUPC()) {
+                cart.erase(cart.begin() + i);
+                return 1;
+            }
+        }
+
+        return 1;
+    }
+}
+
+void ShoppingCart::print() {
+    if (cart.size() == 0) {
+        std::cout << "Your shopping cart is empty." << std::endl;
+    }
+    else {
+        std::cout << "Product\t UPC\t Catagory\t Price\t Quantity" << std::endl;
+        for (int i = 0; i < cart.size(); i++) {
+            cart[i].print();
+            std::cout << "\t " << product_count[cart[i].getUPC()] << std::endl;
+        }
+    }
 }
--- OnlineShoppingCenter/shoppingcart.h
+++ OnlineShoppingCenter/shoppingcart.h
@@ -1,11 +1,29 @@
 #ifndef SHOPPINGCART_H
 #define SHOPPINGCART_H
 
+#include "product.h"
+#include <vector>
+#include <map>
 
 class ShoppingCart
 {
 public:
     ShoppingCart();
+    ShoppingCart(Product p);
+
+    int addProduct(Product p);
+    int deleteProduct(Product p);
+    int deleteProduct(int UPC); // NYI
+    int emptyCart();
+    void print();
+    int checkCart(Product p);
+    int getCount(Product p);
+    int changeQuantity(Product p, int count);
+
+private:
+    std::vector<Product> cart;
+    std::map<int,int> product_count;
+
 };
 
 #endif // SHOPPINGCART_H
