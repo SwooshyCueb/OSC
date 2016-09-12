cd0c1c4bef36e6f20d26f23611d6470ffebdc621
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2016-09-11 20:42:17 -0500

    Pretty up main.cpp a little

--- OnlineShoppingCenter/main.cpp
+++ OnlineShoppingCenter/main.cpp
@@ -6,27 +6,42 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
 
 using namespace std;
 
 static gchar *username = NULL;
 static gchar *password = NULL;
 
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 static GOptionEntry entries[] =
 {
-  { "user", 'u', 0, G_OPTION_ARG_STRING, &username, "Username for logging in (required)", "username" },
-  { "password", 'p', 0, G_OPTION_ARG_STRING, &password, "Password for authentication (required)", "password" },
+  { "user", 'u', 0, G_OPTION_ARG_STRING, &username,
+    "Username for logging in (required)", "username" },
+  { "password", 'p', 0, G_OPTION_ARG_STRING, &password,
+    "Password for authentication (required)", "password" },
   { NULL }
 };
+#pragma GCC diagnostic warning "-Wmissing-field-initializers"
 
 int main(int argc, char *argv[]) {
     GError *error = NULL;
     GOptionContext *context;
 
     // argument parsing
-    context = g_option_context_new("- Online Shopping Center command line interface");
-    g_option_context_set_description(context, "Passwords are beyond the scope of this assignment.\n"
-                                              "You can put pretty much anything for the password and we'll take it.\n");
+    context = g_option_context_new("- Online Shopping Center command line "
+                                   "interface");
+    g_option_context_set_description(context,
+                                     "Passwords are beyond the scope of this "
+                                     "assignment.\n"
+                                     "You can put pretty much anything for the "
+                                     "password and we'll take it.\n"
+                                     "\n"
+                                     "Database files are expected to be in the "
+                                     "current directory.\n"
+                                     "If they are not, they will be created.");
     g_option_context_add_main_entries(context, entries, NULL);
     if (!g_option_context_parse(context, &argc, &argv, &error)) {
         g_print("option parsing failed: %s\n", error->message);
@@ -35,13 +50,15 @@ int main(int argc, char *argv[]) {
 
     if (username == NULL) {
         // user did not provide  username
-        g_printerr("Please provide username. See `%s --help` for more information.\n", argv[0]);
+        g_printerr("Please provide username.\n"
+                   "See `%s --help` for more information.\n", argv[0]);
         exit(1);
     }
 
     if (password == NULL) {
         // user did not provide a password
-        g_printerr("Please provide password. See `%s --help` for more information.\n", argv[0]);
+        g_printerr("Please provide password.\n"
+                   "See `%s --help` for more information.\n", argv[0]);
         exit(1);
     }
 
