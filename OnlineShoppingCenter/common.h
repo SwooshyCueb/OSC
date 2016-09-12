14d677258b3eb40f0f48b0d42610ebcdd80e6862
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2016-09-11 20:39:46 -0500

    Add defines to common.h

--- OnlineShoppingCenter/common.h
+++ OnlineShoppingCenter/common.h
@@ -1,4 +1,17 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <rocksdb/db.h>
+#pragma GCC diagnostic warning "-Wunused-parameter"
+
+#define rocksErr rocksdb::Status
+#define DB rocksdb::DB
+#define DBEntryDescriptor rocksdb::ColumnFamilyDescriptor
+#define DBEntryHandle rocksdb::ColumnFamilyHandle
+#define DBOptions rocksdb::DBOptions
+#define DBEntryOptions rocksdb::ColumnFamilyOptions
+#define DBReadOptions rocksdb::ReadOptions
+#define kDefaultDBEntry rocksdb::kDefaultColumnFamilyName
+
 #endif // COMMON_H
